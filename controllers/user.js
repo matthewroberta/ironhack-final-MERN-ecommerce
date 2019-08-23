8891bbacc55e040ac4f4fffefd319616d83840d5
Matthew Anderson <matthew@mavenly.com>
2019-08-23 00:41:29 -0500

    adds helpers for better error message if duplicate user created and no longer sends salt or hash with new user creation

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const {errorHandler} = require('../helpers/dbErrorHandler')
 
 // export method from controller to use in routes
 exports.signup = (req, res) => {
@@ -8,9 +9,12 @@ exports.signup = (req, res) => {
   user.save((err, user) => {
       if (err) {
           return res.status(400).json({
-              err
+              err: errorHandler(err)
           });
       }
+      // these user.__ = undefined will prevent these things froom passing into saved user
+      user.salt = undefined;
+      user.hashed_password = undefined;
       res.json({
           user
       });
